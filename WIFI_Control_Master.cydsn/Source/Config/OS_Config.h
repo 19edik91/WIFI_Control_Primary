d90238e0cf6c2f9786c0c14362ac6fe7d6c8473a
kraemere <kraemere@8340555f-4234-2d46-9b0e-ce424d8980a8>
2021-05-01 15:26:23 +0000

    Re-located Self-test config
    git-svn-id: file:///C/Users/kraemere/OneDrive/SVN-Repos/WIFI_Control_Master/trunk@10 8340555f-4234-2d46-9b0e-ce424d8980a8

--- WIFI_Control_Master.cydsn/Source/Config/OS_Config.h
+++ WIFI_Control_Master.cydsn/Source/Config/OS_Config.h
@@ -8,14 +8,21 @@
 #ifndef OS_CONFIG_H_
 #define OS_CONFIG_H_
 
+#include "CyLib.h"
+    
 #define MAX_EVENT_TIMER 8
-#define CY_PROJECT_NAME
 
 #define SW_TIMER_10MS       10u
 #define SW_TIMER_51MS       51u
 #define SW_TIMER_251MS      251u
 #define SW_TIMER_1001MS     1001u
 
+#ifdef CY_PROJECT_NAME
+#define EnterCritical()   CyDisableInts()
+#define LeaveCritical(x)  CyEnableInts(x)
+#endif
+    
+    
 //#define VERBOSE   //Enable define for detailed messages
 
 //Use of X-Macros for defining errors
@@ -39,45 +46,5 @@
    ERROR(   eOverTemperatureFault_3                  ,    0xA013      ,       2     ,      FAULTS_DEBOUNCE_CNT_DEFAULT )
 
 //#define TMGR_PostEvent(param1,param2) EVT_PostEvent(eEvtTimer,(uiEventParam1)param1,(ulEventParam2)param2)
-
-
-//*******************************************
-//* Set the selftest actions active         *
-#define SELFTEST_S_ENABLE       1   // The selftests that run once at startup
-
-#if ! SELFTEST_S_ENABLE             // Cyclic selftest may only run if startup test is activated !
-    #define SELFTEST_C_ENABLE   0   // The selftests that run cyclically in main loop
-#else
-    #define SELFTEST_C_ENABLE   1
-#endif
-
-//*******************************************
-//* Defines for inclusion of startup tests  *
-
-#define EXEC_STARTUP_CPUREG             1u          // Execute CPU_Reg test
-#define EXEC_STARTUP_CPUPC              1u          // Execute CPU_PC test
-#define EXEC_STARTUP_TIMEBASE           1u          // Execute Interrupt test
-#define EXEC_STARTUP_RAM                0u          // Execute Ram test
-#define EXEC_STARTUP_STACK              1u          // Execute Stack test
-#define EXEC_STARTUP_FLASH              0u          // Execute Flash test
-#define EXEC_STARTUP_IO                 0u          // Execute IO test
-#define EXEC_STARTUP_UREG               0u          // Execute UDB config register test
-#define EXEC_STARTUP_ADC                0u          // Execute ADC test
-#define EXEC_STARTUP_UART               0u          // Do NOT execute UART test
-
-//*******************************************
-//* Defines for inclusion of cyclic tests   *
-
-#define EXEC_CYCLIC_CPUREG              1u          // Execute CPU_Reg test
-#define EXEC_CYCLIC_CPUPC               1u          // Execute CPU_PC test
-#define EXEC_CYCLIC_TIMEBASE            1u          // Execute Interrupt test
-#define EXEC_CYCLIC_RAM                 0u          // Execute SRAM March tests
-#define EXEC_CYCLIC_STACK               1u          // Execute Stack March tests
-#define EXEC_CYCLIC_STACKOVF            0u          // Execute Stack Overflow test
-#define EXEC_CYCLIC_FLASH               0u          // Execute Flash test
-#define EXEC_CYCLIC_IO                  0u          // Do NOT execute IO test on LV
-#define EXEC_CYCLIC_UREG                0u          // Execute UDB config register test
-#define EXEC_CYCLIC_ADC                 0u          // Do NOT execute ADC test on LV
-#define EXEC_CYCLIC_UART                0u          // Do NOT execute UART test on LV
 
 #endif /* OS_CONFIG_H_ */
