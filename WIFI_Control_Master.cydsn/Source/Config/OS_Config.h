b336a56a23b75b2a0b9c826be27befcc68d64155
kraemere <kraemere@8340555f-4234-2d46-9b0e-ce424d8980a8>
2021-05-04 12:54:57 +0000

    "OS_Config:"  - Added another SW timer
    "StateList:"
     - Added config for additional states
    
    git-svn-id: file:///C/Users/kraemere/OneDrive/SVN-Repos/WIFI_Control_Master/trunk@35 8340555f-4234-2d46-9b0e-ce424d8980a8

--- WIFI_Control_Master.cydsn/Source/Config/OS_Config.h
+++ WIFI_Control_Master.cydsn/Source/Config/OS_Config.h
@@ -12,15 +12,18 @@
     
 #define MAX_EVENT_TIMER 8
 
+#define SW_TIMER_2MS        2u
 #define SW_TIMER_10MS       10u
 #define SW_TIMER_51MS       51u
 #define SW_TIMER_251MS      251u
 #define SW_TIMER_1001MS     1001u
     
-#define EVT_SW_TIMER_10MS   (TIMER_TICK_OFFSET + SW_TIMER_10MS)
-#define EVT_SW_TIMER_51MS   (TIMER_TICK_OFFSET + SW_TIMER_51MS)
-#define EVT_SW_TIMER_251MS   (TIMER_TICK_OFFSET + SW_TIMER_251MS)
-#define EVT_SW_TIMER_1001MS   (TIMER_TICK_OFFSET + SW_TIMER_1001MS)
+    
+#define EVT_SW_TIMER_2MS        (TIMER_TICK_OFFSET + SW_TIMER_2MS)
+#define EVT_SW_TIMER_10MS       (TIMER_TICK_OFFSET + SW_TIMER_10MS)
+#define EVT_SW_TIMER_51MS       (TIMER_TICK_OFFSET + SW_TIMER_51MS)
+#define EVT_SW_TIMER_251MS      (TIMER_TICK_OFFSET + SW_TIMER_251MS)
+#define EVT_SW_TIMER_1001MS     (TIMER_TICK_OFFSET + SW_TIMER_1001MS)
     
     
 #ifdef CY_PROJECT_NAME
@@ -28,10 +31,6 @@
 #define LeaveCritical(x)  CyEnableInts(x)
 #endif
     
-    
-//#define VERBOSE   //Enable define for detailed messages
-
-
 //#define TMGR_PostEvent(param1,param2) EVT_PostEvent(eEvtTimer,(uiEventParam1)param1,(ulEventParam2)param2)
 
 #endif /* OS_CONFIG_H_ */
