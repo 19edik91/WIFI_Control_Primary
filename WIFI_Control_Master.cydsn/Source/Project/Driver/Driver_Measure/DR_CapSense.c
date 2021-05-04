9f4ab2d318993590dc0a3c53bf43a0f3856e060d
kraemere <kraemere@8340555f-4234-2d46-9b0e-ce424d8980a8>
2021-05-04 04:46:34 +0000

    Added driver measurement folder
    git-svn-id: file:///C/Users/kraemere/OneDrive/SVN-Repos/WIFI_Control_Master/trunk@26 8340555f-4234-2d46-9b0e-ce424d8980a8

---
+++ WIFI_Control_Master.cydsn/Source/Project/Driver/Driver_Measure/DR_CapSense.c
@@ -0,0 +1,174 @@
+/* ========================================
+ *
+ * Copyright Eduard Kraemer, 2019
+ *
+ * ========================================
+*/
+#include "CapSense.h"
+#include "Aom.h"
+
+#ifdef CY_SENSE_CapSense_H
+   
+#if (WITHOUT_REGULATION == false)
+/****************************************** Defines ******************************************************/
+typedef enum
+{
+    eCS_SensorScan          = 0x01,
+    eCS_WaitForScanComplete = 0x02,
+    eCS_ProcessData         = 0x03
+}teCapSenseState;
+
+typedef struct
+{
+    bool bOldValue;
+    bool bNewValue;
+    u8 ucWidgetId;
+}tsWidgetValue;
+
+typedef struct
+{
+    tsWidgetValue sWidgetVal_BtnPlus;
+    tsWidgetValue sWidgetVal_BtnMinus;
+    tsWidgetValue sWidgetVal_BtnOnOff;
+}tsWidgetButton;
+
+typedef struct
+{
+    u8 ucLedBrightness;
+    bool bOnOff;
+}tsLedVal;
+
+/****************************************** Variables ****************************************************/
+static teCapSenseState eCS_State = eCS_SensorScan;
+static tsWidgetButton sWidgetButtons;
+static tsLedVal sLedVal;
+
+/****************************************** Function prototypes ******************************************/
+
+/****************************************** loacl functiones *********************************************/
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       20.01.2019
+\fn         MeasureInterrupt()
+\brief      Interrupt function which should be called, whenever a ISR occurs.
+\return     none
+\param      none
+***********************************************************************************/
+void StateMachine(void)
+{
+    switch(eCS_State)
+    {
+        case eCS_SensorScan:
+        {
+            /* Initialize new scan process only when CapSens block is in idle */
+            if(CapSense_IsBusy() == CapSense_NOT_BUSY)
+            {
+                /* Scan the widgets which are configured by the "CSDSetupWidget" API */
+                CapSense_ScanAllWidgets();
+                
+                /* Change state for wait for scan complete */
+                eCS_State = eCS_WaitForScanComplete;
+            }
+            break;
+        }
+        
+        case eCS_WaitForScanComplete:
+        {
+            /* Wait until scan is done */
+            if(CapSense_IsBusy() == CapSense_NOT_BUSY)
+            {
+                /* Switch to next state */
+                eCS_State = eCS_ProcessData;
+            }
+            
+            break;
+        }
+        
+        case eCS_ProcessData:
+        {
+            /* Process data in all enabled widgets */
+            CapSense_ProcessAllWidgets();
+            
+            /* Overwrite old data values */
+            sWidgetButtons.sWidgetVal_BtnMinus.bOldValue = sWidgetButtons.sWidgetVal_BtnMinus.bNewValue;
+            sWidgetButtons.sWidgetVal_BtnPlus.bOldValue = sWidgetButtons.sWidgetVal_BtnPlus.bNewValue;
+            sWidgetButtons.sWidgetVal_BtnOnOff.bOldValue = sWidgetButtons.sWidgetVal_BtnOnOff.bNewValue;
+            
+            /* Get the button data */
+            sWidgetButtons.sWidgetVal_BtnMinus.bNewValue = CapSense_IsWidgetActive(sWidgetButtons.sWidgetVal_BtnMinus.ucWidgetId);
+            sWidgetButtons.sWidgetVal_BtnPlus.bNewValue = CapSense_IsWidgetActive(sWidgetButtons.sWidgetVal_BtnPlus.ucWidgetId);
+            sWidgetButtons.sWidgetVal_BtnOnOff.bNewValue = CapSense_IsWidgetActive(sWidgetButtons.sWidgetVal_BtnOnOff.ucWidgetId);
+            
+            /* Check for changed values */
+            if(sWidgetButtons.sWidgetVal_BtnMinus.bNewValue != sWidgetButtons.sWidgetVal_BtnMinus.bOldValue)
+            {
+                if(sWidgetButtons.sWidgetVal_BtnMinus.bNewValue)
+                {
+                    if(sLedVal.ucLedBrightness >= 10)
+                        sLedVal.ucLedBrightness -= 10;
+                }
+            }
+            
+            if(sWidgetButtons.sWidgetVal_BtnPlus.bNewValue == true 
+                && sWidgetButtons.sWidgetVal_BtnPlus.bNewValue != sWidgetButtons.sWidgetVal_BtnPlus.bOldValue)
+            {
+                if(sWidgetButtons.sWidgetVal_BtnPlus.bNewValue)
+                {
+                    if(sLedVal.ucLedBrightness <= 90)
+                        sLedVal.ucLedBrightness += 10;
+                }
+            }
+            
+            if(sWidgetButtons.sWidgetVal_BtnOnOff.bNewValue != sWidgetButtons.sWidgetVal_BtnOnOff.bOldValue)
+            {
+                if(sWidgetButtons.sWidgetVal_BtnOnOff.bNewValue)
+                    sLedVal.bOnOff ^= 0x01;
+            }
+            
+            break;
+        }
+        
+        default:
+            break;
+    }
+}
+
+
+
+/****************************************** External visible functiones **********************************/
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       13.09.2020
+\fn         CapSense_Init()
+\brief      Initializes cap sense module
+\return     none
+\param      none
+***********************************************************************************/
+void CapSense_Init(void)
+{
+    /* Start cap-sense block */
+    CapSense_Start();
+    
+    sWidgetButtons.sWidgetVal_BtnMinus.ucWidgetId = CapSense_CAPSENSEBTN_MIN_WDGT_ID;
+    sWidgetButtons.sWidgetVal_BtnPlus.ucWidgetId = CapSense_CAPSENSEBTN_PLUS_WDGT_ID;
+    sWidgetButtons.sWidgetVal_BtnOnOff.ucWidgetId = CapSense_CAPSENSEBTN_ONOF_WDGT_ID;
+}
+    
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       13.09.2020
+\fn         CapSense_Handler()
+\brief      Handles the cap sense module
+\return     none
+\param      none
+***********************************************************************************/
+void CapSense_Handler(void)
+{
+    StateMachine();
+}
+
+#endif
+#endif //CY_SENSE_CapSense_H
---
+++ WIFI_Control_Master.cydsn/Source/Project/Driver/Driver_Measure/DR_Measure.c
@@ -0,0 +1,398 @@
+/* ========================================
+ *
+ * Copyright Eduard Kraemer, 2019
+ *
+ * ========================================
+*/
+
+#include "Measure_Voltage.h"
+#include "Measure_Temperature.h"
+#include "Measure_Current.h"
+#include "Measure.h"
+#include "Aom_Measure.h"
+
+#if (WITHOUT_REGULATION == false)
+/****************************************** Defines ******************************************************/
+
+#define ADC_REF_MILLIVOLT        ADC_INPUT_DEFAULT_VREF_MV_VALUE
+#define ADC_INPUT_CHANNEL0       0
+
+#if (AMuxSeq_CHANNELS >= ADC_INPUT_SEQUENCED_CHANNELS_NUM)
+    #define ADC_CHANNELS             AMuxSeq_CHANNELS
+#else
+    #define ADC_CHANNELS             ADC_INPUT_SEQUENCED_CHANNELS_NUM
+#endif
+
+
+/****************************************** Variables ****************************************************/
+/* Fill MUX list with defined outputs correlations */
+static tsAdMuxList sAdMuxList[] = 
+{
+    #define A_CH(ChannelName, sMeasureValue, MeasureType, OutputIndex) {{sMeasureValue}, MeasureType, OutputIndex},
+        AD_MUX_LIST
+    #undef A_CH
+};
+
+static u32 ulSystemVoltageOld;
+static u32 ulSystemVoltageNew;
+static u16 uiSystemVoltageAdc;
+/****************************************** Function prototypes ******************************************/
+static void MeasureInterrupt(void);
+static void PutInMovingAverage(s16 siAdcValue, teAdMuxList eAMuxChannel);
+
+
+/****************************************** loacl functiones *********************************************/
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       20.01.2019
+\brief      Interrupt function which should be called, whenever a ISR occurs.
+\return     none
+\param      none
+***********************************************************************************/
+static void MeasureInterrupt(void)
+{    
+    /* Clear isr flag */
+    #if Measure_ISR    
+        Measure_ISR_ClearPending();
+    #else
+        ADC_INPUT_IRQ_ClearPending();
+        
+        /* Read interrupt status register */
+        u32 intr_status = ADC_INPUT_SAR_INTR_REG;
+        
+        /* Clear handled interrupt */
+        ADC_INPUT_SAR_INTR_REG = intr_status;
+    #endif
+    
+    /* Get the ADC value and put it in the average buffer */
+    PutInMovingAverage(ADC_INPUT_GetResult16(ADC_INPUT_CHANNEL0), AMuxSeq_GetChannel());
+    
+    /* Switch multiplexer to the next channel */
+    AMuxSeq_Next();
+    
+    /* Trigger the conversion */
+    ADC_INPUT_StartConvert();
+}
+
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       20.01.2019
+\brief      Fills a moving average filter with values.
+\return     none
+\param      siAdcValue - New ADC value which should be saved.
+***********************************************************************************/
+static void PutInMovingAverage(s16 siAdcValue, teAdMuxList eAMuxChannel)
+{
+    tsMeasureValues* psMeasureVal = &sAdMuxList[eAMuxChannel].sMeasureValue;
+    
+    /* Subtract the oldest entry from the sum */   
+    psMeasureVal->siAdcSum  -= psMeasureVal->siAdcBuffer[psMeasureVal->ucBufferIndex];
+      
+    /* Save new value in buffer */
+    psMeasureVal->siAdcBuffer[psMeasureVal->ucBufferIndex] = siAdcValue;
+    
+    /* Add new value to summation */
+    psMeasureVal->siAdcSum += siAdcValue;
+    
+    /* Increment buffer index and set back to zero, when limit is reached */
+    if(++psMeasureVal->ucBufferIndex == BUFFER_LENGTH)
+    {
+        psMeasureVal->ucBufferIndex = 0;
+    }
+}
+
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       20.01.2019
+\brief      Calculate average value from the ADC summation value
+\return     uiAdcValue - Returns the calculated ADC value.
+\param      psMeasureValue - Pointer to the measurement structure
+***********************************************************************************/
+static u16 CalculateAveragedAdcValue(const tsMeasureValues* psMeasureValue)
+{
+    u16 uiAdcValue = 0;
+
+    /* Calculate temporary value first */
+    s16 siTempVal = psMeasureValue->siAdcSum / BUFFER_LENGTH;
+    
+    /* Check if temp.val is negative and set to zero if it is */
+    uiAdcValue = (siTempVal < 0) ? 0 : siTempVal; 
+    
+    return uiAdcValue;
+}
+
+
+
+/****************************************** External visible functiones **********************************/
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       20.01.2019
+\brief      Initialize the measurement module.
+\return     none
+\param      none
+***********************************************************************************/
+void Measure_Init(void)
+{
+    /* Clear measure values */   
+    u8 ucMeasureValIdx = 0;
+    for(ucMeasureValIdx = _countof(sAdMuxList); ucMeasureValIdx--;)
+    {
+        if((teAdMuxList)ucMeasureValIdx < eA_CH_INV)
+        {
+            memset(&sAdMuxList[ucMeasureValIdx].sMeasureValue, 0, sizeof(tsMeasureValues));
+        }
+    }
+
+    #if Measure_ISR
+        /* Set ISR address */
+        Measure_ISR_StartEx(MeasureInterrupt);
+        Measure_ISR_Enable();
+    #else        
+        AMuxSeq_Start();
+        while(AMuxSeq_GetChannel < 0)
+        {
+            AMuxSeq_Next();
+        }
+        
+        ADC_INPUT_Start();
+        ADC_INPUT_StartConvert();        
+        ADC_INPUT_IRQ_StartEx(MeasureInterrupt);
+        //ADC_INPUT_SAR_INTR_MASK_REG |= ADC_INPUT_EOS_MASK;
+    #endif
+    
+    /* Activate ADC module */   
+    Measure_Start();
+}
+
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       20.01.2019
+\brief      Calculate voltage from the percentage
+\return     uiVoltageValue - Returns a voltage value in double accuracy (= 1500 => 15.00V)
+\param      ucPercentValue - Value in percent
+***********************************************************************************/
+u32 Measure_CalculateVoltageFromPercent(u8 ucPercentValue, bool bUseDefaultLimit, u8 ucOutputIdx)
+{
+    return Measure_Voltage_CalculateVoltageFromPercent(ucPercentValue, ucOutputIdx, bUseDefaultLimit);
+}
+
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       20.01.2019
+\brief      Calculate ADC value from the voltage value
+\return     uiAdcValue - Returns the calculated ADC value.
+\param      uiVoltage - Voltage value in double accuracy (2400)
+***********************************************************************************/
+u16 Measure_CalculateAdcValue(u32 ulVoltage, u16 uiCurrent)
+{
+    if(ulVoltage)
+    {
+        return Measure_Voltage_CalculateAdcValue(ulVoltage);
+    }    
+    else if(uiCurrent)
+    {
+        return Measure_Current_CalculateAdcValue(uiCurrent);
+    }
+    else
+    {
+        return 0xFFFF;
+    }
+}
+
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       20.01.2019
+\brief      Calculate voltage from the ADC value
+\return     uiVoltage - Voltage value in double accuracy (2400)
+\param      uiAdcValue - Returns the calculated ADC value.
+***********************************************************************************/
+u32 Measure_CalculateVoltageValue(u16 uiAdcValue)
+{   
+    return Measure_Voltage_CalculateVoltageValue(uiAdcValue);
+}
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       08.11.2019
+\brief      Calculate current from the ADC value
+\return     uiCurrent - Current value in milli ampere
+\param      uiAdcValue - Returns the calculated ADC value.
+***********************************************************************************/
+u16 Measure_CalculateCurrentValue(u16 uiAdcValue)
+{   
+    return Measure_Current_CalculateCurrentValue(uiAdcValue);
+}
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       08.11.2019
+\brief      Calculate temperature from the ADC value
+\return     uiTemperature - Temp. value in double accuracy (2400)
+\param      uiAdcValue - Returns the calculated ADC value.
+***********************************************************************************/
+u16 Measure_CalculateTemperatureValue(u16 uiAdcValue)
+{   
+    return Measure_Temperature_CalculateTemperature(uiAdcValue);
+}
+ 
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       20.01.2019
+\brief      This module is called every millisecond.
+\return     none
+\param      none
+***********************************************************************************/
+void Measure_Tick(void)
+{
+    /* Save actual ADC values in AOM */
+    u8 ucAdcChannelIdx;
+    for(ucAdcChannelIdx = _countof(sAdMuxList); ucAdcChannelIdx--;)
+    {    
+        s16 siAvgValue = CalculateAveragedAdcValue(&sAdMuxList[ucAdcChannelIdx].sMeasureValue);
+        
+        /* Voltage ADC is calculated indirectly. The measured voltage is the voltage dissipation
+           over the coil. Therefore the correct ADC value is "MaxAdcVal - ShuntAdcVal - CoilAdcVal = LedAdcVal.
+           For easier calculation the ShuntAdcValue is ignored */
+        if(sAdMuxList[ucAdcChannelIdx].eMeasureType == eMeasureChVoltage)
+        {
+            //Get system voltage
+            ulSystemVoltageNew = Measure_Voltage_GetSystemVoltage();
+            
+            //Check if system voltage changed
+            if(ulSystemVoltageNew != ulSystemVoltageOld)
+            {
+                //WHen changed, calculate new ADC value for system voltage
+                uiSystemVoltageAdc = Measure_Voltage_CalculateAdcValue(ulSystemVoltageNew);
+                ulSystemVoltageOld = ulSystemVoltageNew;
+            }            
+            
+            siAvgValue = uiSystemVoltageAdc - siAvgValue;
+
+            if(siAvgValue < 0)
+                siAvgValue = 0;
+        }
+        
+        /* Calculate average ADC value and save it into AOM */
+        Aom_Measure_SetActualAdcValues(siAvgValue, sAdMuxList[ucAdcChannelIdx].eMeasureType, sAdMuxList[ucAdcChannelIdx].ucOutputIndex);
+    }    
+}
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       10.02.2019
+\brief      Starts the ADC module.
+\return     none
+\param      none
+***********************************************************************************/
+void Measure_Start(void)
+{
+    // Start analog multiplexer. Starts with -1
+    AMuxSeq_Start();
+    while(AMuxSeq_GetChannel() < 0)
+    {
+        AMuxSeq_Next();
+    }    
+    
+    // Set channel mask for no inputs
+    ADC_INPUT_SetChanMask(0xFF);
+    
+    /* Enable ADC-Module */
+    ADC_INPUT_Start();
+    
+    /* Enable the interrupt */
+    //ADC_INPUT_IRQ_Enable();
+    
+    /* Start the first conversion. Every other conversion is
+       started in the interrupt */
+    ADC_INPUT_StartConvert();
+}
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       10.02.2019
+\brief      Stops the ADC module.
+\return     none
+\param      none
+***********************************************************************************/
+void Measure_Stop(void)
+{
+    HAL_Measure_Stop();
+}
+
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       10.11.2019
+\brief      Wrapper function for better modularity
+\return     none
+\param      uiMinLimit - Sets the min limit
+\param      uiMaxLimit - Sets the max limit
+\param      ucOutputIdx - The desired output which shall get new values
+***********************************************************************************/
+void Measure_SetNewVoltageLimits(u32 ulMinLimit, u32 ulMaxLimit, u8 ucOutputIdx)
+{
+    Measure_Voltage_SetNewLimits(ulMinLimit, ulMaxLimit, ucOutputIdx);
+}
+
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       10.11.2019
+\brief      Wrapper function for better modularity
+\return     u32 - SystemVoltage in millivolt
+\param      none
+***********************************************************************************/
+u32 Measure_GetSystemVoltage(void)
+{
+    return Measure_Voltage_GetSystemVoltage();
+}
+
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       10.11.2019
+\brief      Wrapper function for better modularity
+\param     u32 - System voltage in millivolt
+\return    none
+***********************************************************************************/
+void Measure_SetSystemVoltage(u32 ulSystemVoltage)
+{
+    Measure_Voltage_SetSystemVoltage(ulSystemVoltage);
+}
+
+
+//********************************************************************************
+/*!
+\author     Kraemer E.
+\date       27.12.2020
+\brief      Function which returns the averaged ADC value of the desired channel
+\return     u16 - Averaged ADC value
+\param      eAdcChannel - The ADC channel with the values 
+***********************************************************************************/
+u16 Measure_GetAveragedAdcValue(teAdMuxList eAdcChannel)
+{
+    return CalculateAveragedAdcValue(&sAdMuxList[eAdcChannel].sMeasureValue);
+}
+#endif
